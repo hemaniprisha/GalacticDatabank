2cf94fa2b7d7e3c87cc7e6363bf1be0aaa04e0b9
hemaniprisha <hemaniprisha1@gmail.com>
2025-11-19 03:16:03 -0800

    Initial Commit

---
+++ GalacticArchives/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalacticArchives
+//
+//  Created by Prisha Hemani on 11/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GalacticArchivesTests/GalacticArchivesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GalacticArchivesTests.swift
+//  GalacticArchivesTests
+//
+//  Created by Prisha Hemani on 11/19/25.
+//
+
+import Testing
+@testable import GalacticArchives
+
+struct GalacticArchivesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
